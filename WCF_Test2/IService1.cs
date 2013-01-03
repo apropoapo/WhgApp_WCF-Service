128fd0688c75b53dd508102439a9f3e09b6755d5
apropoapo <apropoapo@hotmail.de>
2013-01-03 17:56:14 +0100

    0.2.9 komplett neu WCF web service

---
+++ WCF_Test2/IService1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Test2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int plus(int v1, int v2);
+
+  
+
+        // TODO: Add your service operations here
+    }
+
+
+
+}
